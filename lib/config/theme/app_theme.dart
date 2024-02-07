853a9f43927087079f5af1ca24d33d28fdb4a29d
Luis Salcido <salcidogrijalva@outlook.com>
2024-02-07 15:20:03 -0700

    App Theme added

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colors = [
+  Colors.blue,
+  Colors.teal,
+  Colors.red,
+  Colors.purple,
+  Colors.pinkAccent
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0, 'Selected color must be equal or greater than 0'),
+      assert(selectedColor < colors.length, 'Selected color must be less or equal than ${colors.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    colorSchemeSeed: colors[selectedColor],
+    useMaterial3: true
+  );
+}
